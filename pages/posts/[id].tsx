478ac3b75ee7592bff806cab78705637293c7d60
d.kanai <contact@dkanai.com>
2022-09-10 07:42:47 +0900

    fix ts

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -6,7 +6,7 @@ import { getAllPostIds, getPostData } from '../../libs/posts';
 
 export default function Post({ postData }) {
   return (
-    <Layout>
+    <Layout home={false}>
       <Head>
         <title>{postData.title}</title>
       </Head>
