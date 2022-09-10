f77b5836657bcf4f3525325efd589ed767a5e6fb
d.kanai <contact@dkanai.com>
2022-09-10 19:32:16 +0900

    guard

--- backend/src/cats/cats.controller.ts
+++ backend/src/cats/cats.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Query, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { UseGuards, Controller, Get, Query, Post, Body, Put, Param, Delete } from '@nestjs/common';
 import { CreateCatDto, UpdateCatDto, ListAllEntities } from './cats.dto';
 import { CatsService } from './cats.service';
 import { ParseIntPipe, HttpStatus, HttpException } from '@nestjs/common';
+import { RolesGuard } from 'src/common/guard/roles.guard';
 
 @Controller('cats')
 export class CatsController {
@@ -9,12 +10,16 @@ export class CatsController {
 
   @Post()
   create(@Body() createCatDto: CreateCatDto) {
+    console.log(createCatDto)
+    console.log(createCatDto.name)
     this.catsService.create(createCatDto);
   }
 
   @Get()
+  @UseGuards(RolesGuard)
   findAll(@Query() query: ListAllEntities) {
-    throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
+    return this.catsService.findAll()
+    // throw new HttpException('Forbidden', HttpStatus.FORBIDDEN);
   }
 
   @Get(':id')
@@ -30,5 +35,10 @@ export class CatsController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return `This action removes a #${id} cat`;
+  }
+
+  @Post()
+  createBulk(@Body() createCatDtos: CreateCatDto[]) {
+    return 'This action adds new users';
   }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './common/http-exception.filter'
+import { HttpExceptionFilter } from './common/filter/http-exception.filter'
 import { ValidationPipe} from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.useGlobalFilters(new HttpExceptionFilter());
-  app.useGlobalPipes(new ValidationPipe({whitelist: true}));
+  app.useGlobalPipes(new ValidationPipe({whitelist: true, transform: true}));
   await app.listen(3000);
 }
 bootstrap();
