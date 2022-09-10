6635de618c66fccd7d125a06e3f7cfe3078dc64c
d.kanai <contact@dkanai.com>
2022-09-10 18:57:59 +0900

    handler class-validator

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/http-exception.filter'
+import { ValidationPipe} from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalPipes(new ValidationPipe({whitelist: true}));
   await app.listen(3000);
 }
 bootstrap();
