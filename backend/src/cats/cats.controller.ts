04e2152549c089ba94fe9770b61d659167e36a70
d.kanai <contact@dkanai.com>
2022-09-10 11:28:50 +0900

    module split

--- backend/src/cats/cats.controller.ts
+++ backend/src/cats/cats.controller.ts
@@ -1,16 +1,19 @@
 import { Controller, Get, Query, Post, Body, Put, Param, Delete } from '@nestjs/common';
 import { CreateCatDto, UpdateCatDto, ListAllEntities } from './cats.dto';
+import { CatsService } from './cats.service';
 
 @Controller('cats')
 export class CatsController {
+  constructor(private catsService: CatsService) {}
+
   @Post()
   create(@Body() createCatDto: CreateCatDto) {
-    return 'This action adds a new cat';
+    this.catsService.create(createCatDto);
   }
 
   @Get()
   findAll(@Query() query: ListAllEntities) {
-    return `This action returns all cats (limit: ${query.limit} items)`;
+    return this.catsService.findAll();
   }
 
   @Get(':id')
