4d6041218bb209298c10c9511e778b85dc87e337
d.kanai <contact@dkanai.com>
2022-09-17 19:16:25 +0900

    header in layout and ThemeProvider

--- frontend/components/layout.tsx
+++ frontend/components/layout.tsx
@@ -1,9 +1,18 @@
 import Head from 'next/head';
+import { AppBar, Avatar, Badge, Box, IconButton, Toolbar, Tooltip } from '@mui/material';
+import styled from '@emotion/styled';
+import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
 import styles from './layout.module.css';
 import Link from 'next/link';
+import { Theme } from '@mui/material/styles';
 
-const name = 'd.kanai';
 export const siteTitle = 'Next.js Sample Website';
+
+const DashboardNavbarRoot = styled(AppBar)(( theme:Theme) => ({
+  // backgroundColor: theme.palette.background.paper,
+  // boxShadow: theme.shadows[3]
+}));
 
 export default function Layout({ children, home }) {
   return (
@@ -15,6 +24,35 @@ export default function Layout({ children, home }) {
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
+      <DashboardNavbarRoot sx={{ left: { lg: 280 }, width: { lg: 'calc(100% - 280px)' } }} >
+        <Toolbar disableGutters sx={{ minHeight: 64, left: 0, px: 2 }} >
+          <IconButton onClick={() => {}} sx={{ display: { xs: 'inline-flex', lg: 'none' } }} >
+            <MenuIcon fontSize="small" />
+          </IconButton>
+          <Tooltip title="Search">
+            <IconButton sx={{ ml: 1 }}>
+              <SearchIcon fontSize="small" />
+            </IconButton>
+          </Tooltip>
+          <Box sx={{ flexGrow: 1 }} />
+          <Tooltip title="Contacts">
+            <IconButton sx={{ ml: 1 }}>
+              {/* <UsersIcon fontSize="small" /> */}
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Notifications">
+            <IconButton sx={{ ml: 1 }}>
+              <Badge badgeContent={4} color="primary" variant="dot" >
+                {/* <BellIcon fontSize="small" /> */}
+              </Badge>
+            </IconButton>
+          </Tooltip>
+          {/* <Avatar onClick={() => setOpenAccountPopover(true)} ref={settingsRef} sx={{ cursor: 'pointer', height: 40, width: 40, ml: 1 }} src="/static/images/avatars/avatar_1.png" > */}
+            {/* <UserCircleIcon fontSize="small" /> */}
+          {/* </Avatar> */}
+        </Toolbar>
+      </DashboardNavbarRoot>
+      {/* <AccountPopover anchorEl={settingsRef.current} open={openAccountPopover} onClose={() => setOpenAccountPopover(false)} /> */}
       <main>{children}</main>
       {!home && (
         <div className={styles.backToHome}>
