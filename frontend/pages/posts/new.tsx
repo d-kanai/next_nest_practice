70931cc24d1c9066afe0f41903f66ac5618d3fe0
d.kanai <contact@dkanai.com>
2022-09-17 13:09:17 +0900

    zod

--- frontend/pages/posts/new.tsx
+++ frontend/pages/posts/new.tsx
@@ -1,12 +1,19 @@
 import utilStyles from '../../styles/utils.module.css';
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
 import Layout from '../../components/layout';
 import { useForm } from "react-hook-form";
 import { useMutation } from '@apollo/client';
 import { CreatePostDocument } from '../../graphql/generated/graphql';
 import { useRouter } from 'next/router'
 
+const schema = zod.object({
+  title: zod.string().min(1, { message: "Required" }),
+  authorId: zod.string().min(1, { message: "Required" }),
+});
+
 export default function Posts({ postData }) {
-  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const { register, handleSubmit, watch, formState: { errors } } = useForm({resolver: zodResolver(schema)});
   const [ mutate, { data, loading, error }] = useMutation(CreatePostDocument);
   const router = useRouter()
 
@@ -21,10 +28,10 @@ export default function Posts({ postData }) {
       <h2 className={utilStyles.headingLg}>New Posts</h2>
       <form onSubmit={handleSubmit(onSubmit)}>
       <input defaultValue="test" {...register("title", { required: true })} />
-      {errors.title && <span>This field is required</span>}
+      {errors.title?.message && <span>{errors.title?.message.toString()}</span>}
       <br/>
       <input defaultValue="1" {...register("authorId", { required: true })} />
-      {errors.authorId && <span>This field is required</span>}
+      {errors.authorId?.message && <span>{errors.authorId?.message.toString()}</span>}
       <br/>
       <input type="submit" />
     </form>
